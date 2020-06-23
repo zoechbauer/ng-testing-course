83d4915993a9bb9001ed3cd52caaea5a80d05ab3
Zoechbauer <hans.zoechbauer@aon.at>
2020-06-23 13:52:13 +0200

    UT CoursesService: Unit tests for data retrieval

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -4,12 +4,14 @@ import {
   HttpClientTestingModule,
 } from '@angular/common/http/testing';
 import { CoursesService } from './courses.service';
+import { COURSES } from '../../../../server/db-data';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
+    console.log('CoursesService: calling beforeEach');
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [CoursesService],
@@ -19,7 +21,40 @@ describe('CoursesService', () => {
     coursesService = TestBed.get(CoursesService);
   });
 
-  it('should find all courses', () => {
-    pending();
+  it('should retrieve all courses', () => {
+    console.log('CoursesService: retrieve all courses test');
+    coursesService.findAllCourses().subscribe((courses) => {
+      // console.log('courses', courses);
+      expect(courses).toBeTruthy('No courses returned');
+
+      expect(courses.length).toBe(12, 'incorrect number of courses');
+
+      const course12 = courses.find((course) => course.id === 12);
+
+      expect(course12.titles.description).toBe(
+        'Angular Testing Course',
+        'wrong title description'
+      );
+    });
+
+    const req = httpTestingController.expectOne('/api/courses');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush({ payload: Object.values(COURSES) });
+  });
+
+  it('should retrieve course by id', () => {
+    coursesService.findCourseById(12).subscribe((course) => {
+      // console.log('course', course);
+      expect(course).toBeTruthy('cannot find course');
+      expect(course.id).toBe(12, 'wrong course id');
+    });
+
+    const req = httpTestingController.expectOne('/api/courses/12');
+
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(COURSES[12]);
   });
 });
