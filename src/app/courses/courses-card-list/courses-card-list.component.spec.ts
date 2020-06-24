34981584b9051a0e9d02b62af86e55e9280d2f9b
Zoechbauer <hans.zoechbauer@aon.at>
2020-06-24 18:23:13 +0200

    UT Components: CoursesCardList - Unit test for display course list (search DOM)

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -11,6 +11,7 @@ import { setupCourses } from '../common/setup-test-data';
 describe('CoursesCardListComponent', () => {
   let component: CoursesCardListComponent;
   let fixture: ComponentFixture<CoursesCardListComponent>;
+  let el: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -20,6 +21,7 @@ describe('CoursesCardListComponent', () => {
       .then(() => {
         fixture = TestBed.createComponent(CoursesCardListComponent);
         component = fixture.componentInstance;
+        el = fixture.debugElement;
       });
   }));
 
@@ -29,7 +31,16 @@ describe('CoursesCardListComponent', () => {
   });
 
   it('should display the course list', () => {
-    pending();
+    component.courses = setupCourses();
+
+    fixture.detectChanges();
+
+    const cards = el.queryAll(By.css('.course-card'));
+    // console.log('cards', cards);
+    // console.log('debugElement', el.nativeElement.outerHTML);
+
+    expect(cards).toBeTruthy('no cards');
+    expect(cards.length).toBe(12, 'wrong number of cards');
   });
 
   it('should display the first course', () => {
