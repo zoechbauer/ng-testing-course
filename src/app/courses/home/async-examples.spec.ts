d04751cca105ffbfe927ee81918e23f4e5dae3d1
Zoechbauer <hans.zoechbauer@aon.at>
2020-06-29 13:10:08 +0200

    Async Examples: fakeAsync & tick

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,5 +1,7 @@
+import { fakeAsync, flush, tick } from '@angular/core/testing';
+
 fdescribe('Async Testing Examples', () => {
-  it('Asynchronous test examole with Jasmine done', (done: DoneFn) => {
+  it('Asynchronous test example with Jasmine done', (done: DoneFn) => {
     let test = false;
 
     setTimeout(() => {
@@ -9,4 +11,21 @@ fdescribe('Async Testing Examples', () => {
       done();
     }, 1000);
   });
+
+  fit('Asynchronous test example with setTimeout: fakeAsync & tick', fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {
+      console.log('running assertions');
+      test = true;
+    }, 1000);
+
+    tick(500);
+    console.log('some computations');
+    tick(499);
+    console.log('some computations');
+    tick(1);
+
+    expect(test).toBeTruthy();
+  }));
 });
