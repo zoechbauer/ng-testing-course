222bf5d0837b592df548b795fafdecf20e9b9d42
Zoechbauer <hans.zoechbauer@aon.at>
2020-07-06 15:28:13 +0200

    Home Page: Should display Advanced Courses

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -2,7 +2,7 @@
 /// <reference types="cypress" />
 
 describe('Home Page', () => {
-  it('should display a list of courses', () => {
+  beforeEach(() => {
     cy.fixture('courses.json').as('coursesJSON');
 
     cy.server();
@@ -10,11 +10,27 @@ describe('Home Page', () => {
     cy.route('/api/courses', '@coursesJSON').as('courses');
 
     cy.visit('/');
+  });
 
+  it('should display a list of courses', () => {
     cy.contains('All Courses');
 
     cy.wait('@courses');
 
     cy.get('mat-card').should('have.length', 9);
+  });
+
+  it('should display Advanced Courses', () => {
+    cy.get('.mat-tab-label').should('have.length', 2);
+
+    cy.get('.mat-tab-label').last().click();
+
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .its(length)
+      .should('be.gt', 1);
+
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .first()
+      .should('contain', 'Angular Security Course');
   });
 });
